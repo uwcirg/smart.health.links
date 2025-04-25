4ee5b4daa4029093bf3ed1b5cea6d1f129c77fd5
Daniel Lorigan <daniellrgn@gmail.com>
2025-04-25 16:18:10 -0700

    Remove debug and simplify entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 set -e
 
-# Check if /app/db exists and is owned by root
-if [ -d /app/db ]; then
-  current_owner=$(stat -c "%u" /app/db)
-  if [ "$current_owner" -eq 0 ]; then
-    echo "[entrypoint] Fixing ownership of /app/db..."
-    chown -R deno:deno /app/db
-  fi
-fi
-
-echo ">> Running as: $(whoami)"
-echo ">> Command: $@"
+chown -R deno:deno /app/db
 
 # Drop privileges to deno and run your command
 exec gosu deno "$@"
